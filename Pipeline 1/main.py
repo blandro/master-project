4dc9b82eeb7193c0ecf7ab674021d68d18c3de03
Benjamin Landro <landro.benjamin@gmail.com>
2025-02-24 14:15:34 +0100

    Make more readable script for Pipeline 1

--- Pipeline 1/main.py
+++ Pipeline 1/main.py
@@ -7,32 +7,33 @@ def run_blast(user_fasta, evalue, identity, num_threads=8):
     
     # Run BLAST
     blast_cmd = f"blastp -query {user_fasta} -db transporters_db -outfmt 6 -evalue {evalue} -num_threads {num_threads} -out results.txt"
-    subprocess.run(blast_cmd, shell=True)
+    subprocess.run(blast_cmd, shell=True, check=True)
 
     # Parse results
-    blast_results = pd.read_csv("results.txt", sep="\t", header=None)
-    blast_results.columns = ["QueryID", "SubjectID", "Identity", "AlignLength", 
-                             "Mismatches", "GapOpens", "QStart", "QEnd", 
-                             "SStart", "SEnd", "Evalue", "BitScore"]
+    column_names = ["QueryID", "SubjectID", "Identity", "AlignLength", 
+                    "Mismatches", "GapOpens", "QStart", "QEnd", 
+                    "SStart", "SEnd", "Evalue", "BitScore"]
+    blast_results = pd.read_csv("results.txt", sep="\t", names=column_names, low_memory=False)
 
     # Filter results
-    filtered_hits = blast_results[
+    filtered_hits = blast_results.loc[
         (blast_results["Evalue"] <= evalue) & 
         (blast_results["Identity"] >= identity)
-    ]
-    filtered_hits_cp = filtered_hits.copy()
-    filtered_hits_cp[["UID", "TCID"]] = filtered_hits["SubjectID"].str.split("|", expand=True)
-    filtered_hits_cp = filtered_hits_cp.drop(columns=["SubjectID"])
-    cols = ["UID", "TCID"] + [col for col in filtered_hits_cp.columns if col not in ["UID", "TCID"]]
-    filtered_hits_cp = filtered_hits_cp[cols]
-    return filtered_hits_cp
+    ].copy()
 
-def extract_reactions(filtered_hits):
+    # Extract UID and TCID from SubjectID
+    filtered_hits[["UID", "TCID"]] = filtered_hits["SubjectID"].str.split("|", expand=True)
+    filtered_hits.drop(columns=["SubjectID"], inplace=True)
+
+    # Reorder columns
+    reordered_columns = ["UID", "TCID"] + [col for col in filtered_hits.columns if col not in ["UID", "TCID"]]
+    filtered_hits = filtered_hits[reordered_columns]
+
     """Extract reactions from DataFrame based on matched sequence IDs."""
     df = pd.read_csv("transporters_df.tsv", sep="\t")
     results = pd.merge(df, filtered_hits, on=["UID", "TCID"], how="inner")
     results.to_csv("identified_proteins.tsv", sep="\t", index=False)
-    print("Done BLASTing!")
+    print("Done - The BLAST is in the past!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="BLAST pipeline for transporter proteins")
@@ -44,5 +45,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Run BLAST and extract reactions
-    filtered_hits = run_blast(args.user_fasta, args.evalue, args.identity, args.num_threads)
-    extract_reactions(filtered_hits)
+    run_blast(args.user_fasta, args.evalue, args.identity, args.num_threads)
