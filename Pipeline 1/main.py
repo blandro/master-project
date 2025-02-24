82d042f28491dcd7f2bc8fc62ef9e1843708323c
Benjamin Landro <landro.benjamin@gmail.com>
2025-02-24 13:35:52 +0100

    Pipeline 1 description and improvement

--- Pipeline 1/main.py
+++ Pipeline 1/main.py
@@ -2,11 +2,11 @@ import argparse
 import subprocess
 import pandas as pd
 
-def run_blast(user_fasta, reference_db, evalue, identity, num_threads=8):
+def run_blast(user_fasta, evalue, identity, num_threads=8):
     """Run BLASTp against reference and filter results."""
     
     # Run BLAST
-    blast_cmd = f"blastp -query {user_fasta} -db {reference_db} -outfmt 6 -evalue {evalue} -num_threads {num_threads} -out results.txt"
+    blast_cmd = f"blastp -query {user_fasta} -db transporters_db -outfmt 6 -evalue {evalue} -num_threads {num_threads} -out results.txt"
     subprocess.run(blast_cmd, shell=True)
 
     # Parse results
@@ -20,24 +20,23 @@ def run_blast(user_fasta, reference_db, evalue, identity, num_threads=8):
         (blast_results["Evalue"] <= evalue) & 
         (blast_results["Identity"] >= identity)
     ]
-    filtered_hits[["UID", "TCID"]] = filtered_hits["QueryID"].str.split("|", expand=True)
-    filtered_hits = filtered_hits.drop(columns=['QueryID'])
-    cols = ['UID', 'TCID'] + [col for col in filtered_hits.columns if col not in ['UID', 'TCID']]
-    filtered_hits = filtered_hits[cols]
-    return filtered_hits
+    filtered_hits_cp = filtered_hits.copy()
+    filtered_hits_cp[["UID", "TCID"]] = filtered_hits["SubjectID"].str.split("|", expand=True)
+    filtered_hits_cp = filtered_hits_cp.drop(columns=["SubjectID"])
+    cols = ["UID", "TCID"] + [col for col in filtered_hits_cp.columns if col not in ["UID", "TCID"]]
+    filtered_hits_cp = filtered_hits_cp[cols]
+    return filtered_hits_cp
 
-def extract_reactions(filtered_hits, df_path):
+def extract_reactions(filtered_hits):
     """Extract reactions from DataFrame based on matched sequence IDs."""
-    df = pd.read_csv(df_path, sep="\t")
+    df = pd.read_csv("transporters_df.tsv", sep="\t")
     results = pd.merge(df, filtered_hits, on=["UID", "TCID"], how="inner")
-    results.to_csv("Identified_reactions.tsv", sep="\t", index=False)
-    print("Done!")
+    results.to_csv("identified_proteins.tsv", sep="\t", index=False)
+    print("Done BLASTing!")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="BLAST pipeline for transporter proteins")
     parser.add_argument("--user_fasta", required=True, help="User-provided genome FASTA")
-    parser.add_argument("--reference_db", required=True, help="Reference protein database")
-    parser.add_argument("--df_path", required=True, help="Path to transport reaction DataFrame TSV")
     parser.add_argument("--evalue", type=float, default=1e-5, help="E-value threshold")
     parser.add_argument("--identity", type=float, default=30.0, help="Identity threshold")
     parser.add_argument("--num_threads", type=int, default=8, help="Number of CPU threads for BLAST")
@@ -45,5 +44,5 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Run BLAST and extract reactions
-    filtered_hits = run_blast(args.user_fasta, args.reference_db, args.evalue, args.identity, args.num_threads)
-    extract_reactions(filtered_hits, args.df_path)
+    filtered_hits = run_blast(args.user_fasta, args.evalue, args.identity, args.num_threads)
+    extract_reactions(filtered_hits)
